539aa17fa40d3cb7fc7151963b133bdf017ed2f2
MindRoad AB <38464472+MindRoadAB@users.noreply.github.com>
2023-03-24 11:37:39 +0100

    Added updated CLUtil

--- cl-test/clUtil.h
+++ cl-test/clUtil.h
@@ -1,11 +1,38 @@
+#include <CL/opencl.hpp>
 #include <chrono>
 #include <fstream>
 #include <iostream>
 
-std::string const DEFAULT_PATH = "./";
-std::string get_kernel_func(std::string &&file_name,
-                            std::string &&path = "NULL") {
-  if (path == "NULL") {
+inline cl::Device get_default_device() {
+  // get all platforms (drivers)
+  std::vector<cl::Platform> all_platforms;
+  cl::Platform::get(&all_platforms);
+  if(all_platforms.size() == 0) {
+    std::cout << " No platforms found. Check OpenCL installation!\n";
+    exit(1);
+  }
+  cl::Platform default_platform = all_platforms[0];
+  std::cout << "Using platform: "
+            << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+
+  // get default device of the default platform
+  std::vector<cl::Device> all_devices;
+  default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+  if(all_devices.size() == 0) {
+    std::cout << " No devices found. Check OpenCL installation!\n";
+    exit(1);
+  }
+  cl::Device default_device = all_devices[0];
+  std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>()
+            << "\n";
+
+  return default_device;
+}
+
+std::string const DEFAULT_PATH = "cl_kern/";
+inline std::string get_kernel_func(std::string &&file_name,
+                                   std::string &&path = "NULL") {
+  if(path == "NULL") {
     path = DEFAULT_PATH;
   }
   std::ifstream t(path + file_name);
@@ -15,14 +42,16 @@ std::string get_kernel_func(std::string &&file_name,
   return tmp;
 }
 
-auto get_time() -> decltype(std::chrono::high_resolution_clock::now()) {
+inline auto get_time() -> decltype(std::chrono::high_resolution_clock::now()) {
   auto tmp{std::chrono::high_resolution_clock::now()};
   return tmp;
 }
 
-// void print_time(std::ostream &os, auto start, auto stop) {
-//   os << "Time in ms: "
-//      << std::chrono::duration_cast<std::chrono::microseconds>(stop - start)
-//             .count()
-//      << std::endl;
-// }
+using timePoint = std::chrono::high_resolution_clock::time_point;
+
+inline void print_time(std::ostream &os, timePoint start, timePoint stop) {
+  os << "Time in ms: "
+     << std::chrono::duration_cast<std::chrono::microseconds>(stop - start)
+            .count()
+     << std::endl;
+}
