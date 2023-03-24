448b505742fbd8b2355bfd749af057ea564a6481
MindRoad AB <38464472+MindRoadAB@users.noreply.github.com>
2023-03-24 11:35:49 +0100

    small wip convolve take valarray

--- cl-test/main.cpp
+++ cl-test/main.cpp
@@ -2,6 +2,7 @@
 
 #include "argsUtil.h"
 #include "fitsUtil.h"
+#include "clUtil.h"
 
 int main(int argc, char* argv[]) {
   CCfits::FITS::setVerboseMode(true);
@@ -21,6 +22,25 @@ int main(int argc, char* argv[]) {
   }
   
   readImage(templateImg);
+
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+    if(all_platforms.size()==0){
+        std::cout<<" No platforms found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Platform default_platform=all_platforms[0];
+    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
+
+    //get default device of the default platform
+    std::vector<cl::Device> all_devices;
+    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+    if(all_devices.size()==0){
+        std::cout<<" No devices found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Device default_device=all_devices[0];
+    std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
 
   Image outImg{args.outName, args.outPath, templateImg.data, templateImg.axis};
   writeImage(outImg);
