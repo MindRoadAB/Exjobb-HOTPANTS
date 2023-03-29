74f15658c02a750dbf801379d711b758945697ad
MindRoad AB <38464472+MindRoadAB@users.noreply.github.com>
2023-03-29 15:03:04 +0200

    first stamp struct

--- BACH/utils/bachUtil.h
+++ BACH/utils/bachUtil.h
@@ -1,8 +1,61 @@
 #ifndef BACH_UTIL
 #define BACH_UTIL
 
-#include <iostream>
 #include <CL/opencl.hpp>
+#include <iostream>
+#include <utility>
+
+struct Stamp {
+  cl_double* stampData{};
+  std::pair<cl_int, cl_int> stampCoords{};
+  std::pair<cl_int, cl_int> stampSize{};
+  std::pair<cl_int, cl_int> subStampCoords{};
+
+  ~Stamp() { delete stampData; }
+  Stamp(cl_double* stampData, std::pair<cl_int, cl_int> stampCoords,
+        std::pair<cl_int, cl_int> stampSize,
+        std::pair<cl_int, cl_int> subStampCoords)
+      : stampCoords{stampCoords},
+        stampSize{stampSize},
+        subStampCoords{subStampCoords} {
+    for(cl_int i = 0; i < stampSize.first * stampSize.second; i++) {
+      this->stampData[i] = stampData[i];
+    }
+  }
+
+  Stamp(const Stamp& other)
+      : stampCoords{other.stampCoords},
+        stampSize{other.stampSize},
+        subStampCoords{other.subStampCoords} {
+    for(cl_int i = 0; i < stampSize.first * stampSize.second; i++) {
+      this->stampData[i] = other.stampData[i];
+    }
+  }
+
+  Stamp(Stamp&& other)
+      : stampData{std::exchange(other.stampData, nullptr)},
+        stampCoords{other.stampCoords},
+        stampSize{other.stampSize},
+        subStampCoords{other.subStampCoords} {}
+
+  Stamp& operator=(const Stamp& other) {
+    this->stampCoords = other.stampCoords;
+    this->stampSize = other.stampSize;
+    this->subStampCoords = other.subStampCoords;
+    for(cl_int i = 0; i < stampSize.first * stampSize.second; i++) {
+      this->stampData[i] = other.stampData[i];
+    }
+    return *this;
+  }
+
+  Stamp& operator=(Stamp&& other) {
+    this->stampCoords = other.stampCoords;
+    this->stampSize = other.stampSize;
+    this->subStampCoords = other.subStampCoords;
+    stampData = std::exchange(other.stampData, nullptr);
+    return *this;
+  }
+};
 
 inline void checkError(cl_int err) {
   if(err != 0) {
