24bf262688a8508fd02e1dd5152ff198fdd14d6d
MindRoad AB <38464472+MindRoadAB@users.noreply.github.com>
2023-04-06 11:30:13 +0200

    add threshold lowering if not enough sstamps found

--- BACH/main.cpp
+++ BACH/main.cpp
@@ -62,11 +62,29 @@ int main(int argc, char* argv[]) {
               << std::endl;
 
   identifySStamps(templateStamps, templateImg);
+  int hasSStamps = 0;
+  for(auto s : templateStamps) {
+    if(s.subStamps.empty()) hasSStamps++;
+  }
+  if(hasSStamps / templateStamps.size() < 0.1) {
+    args.threshLow *= 0.5;
+    identifySStamps(templateStamps, templateImg);
+    args.threshLow /= 0.5;
+  }
   if(args.verbose)
     std::cout << "Substamps found in " << templateImg.name << std::endl
               << std::endl;
 
   identifySStamps(sciStamps, scienceImg);
+  hasSStamps = 0;
+  for(auto s : sciStamps) {
+    if(s.subStamps.empty()) hasSStamps++;
+  }
+  if(hasSStamps / sciStamps.size() < 0.1) {
+    args.threshLow *= 0.5;
+    identifySStamps(sciStamps, scienceImg);
+    args.threshLow /= 0.5;
+  }
   if(args.verbose)
     std::cout << "Substamps found in " << scienceImg.name << std::endl
               << std::endl;
